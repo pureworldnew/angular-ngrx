18480d383fcdb73cc05fae985de3a81d0f92b51f
Pure World <pureworldnew@gmail.com>
2023-11-20 19:41:55 -0600

    add my-counter ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCounterComponent
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot({ count: counterReducer }),
     AppRoutingModule
   ],
   providers: [],
